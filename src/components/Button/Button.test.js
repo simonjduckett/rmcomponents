0b5342d094c886e91094d928065d9ae223b4a1d6
simonduckett <simonjduckett@hotmail.co.uk>
2022-08-31 12:22:12 +0100

    setup testing library

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -3,4 +3,6 @@ import { render, screen } from "@testing-library/react"
 
 test('should render', () => {
     render(<Button label='click me' />)
+    expect.stringContaining('click')
+    expect(screen.getByRole('button')).toHaveTextContent('click me')
 })
