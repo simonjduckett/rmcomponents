5f6d2cb60d79abc9ad84f85cc22289f3624a0e9b
simonduckett <simonjduckett@hotmail.co.uk>
2022-09-02 11:41:05 +0100

    testing updates

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,8 +1,14 @@
 import Button from "./index"
+import { Primary } from "./Button.stories"
 import { render, screen } from "@testing-library/react"
 
 test('should render', () => {
     render(<Button label='click me' />)
     expect.stringContaining('click')
-    expect(screen.getByRole('button')).toHaveTextContent('click me')
+    expect(screen.getByRole('button')).toHaveTextContent('click')
+})
+
+test('should render primary', () => {
+    render(<Primary {...Primary.args} />)
+    expect(screen.getByRole('button')).toHaveTextContent('See quotes')
 })
