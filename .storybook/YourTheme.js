c1b5814b790821ce5177bde38dd247e34c9e0972
simonduckett <simonjduckett@hotmail.co.uk>
2022-11-15 15:44:49 +0000

    add custom theme file

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'ReallyMoving Style Guide',
+    brandUrl: 'https://reallymoving.com',
+    brandImage: 'https://www.reallymoving.com/images/logo/rm-logo-vector-svg.svg',
+    brandTarget: '_self',
+});
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,7 +2,8 @@
 
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
+import yourTheme from './YourTheme';
 
 addons.setConfig({
-    theme: themes.dark,
+    theme: yourTheme,
 });
