93a73106b43c92c526d20a681a871a000368c2b6
simonduckett <simonjduckett@hotmail.co.uk>
2022-12-22 16:57:01 +0000

    update navbar comps

--- src/components/NavDropdown/NavDropdown.js
+++ src/components/NavDropdown/NavDropdown.js
@@ -4,7 +4,7 @@ import styles from './NavDropdown.module.scss'
 const NavDropdown = props => {
     return ( 
         <li className={styles.NavDropdown}>
-            <a>{props.title}</a>
+            <a className={props.icon ? styles.icon : null}>{props.title}</a>
             <div className={styles.dropdown}>{props.children}</div>
         </li>
      );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types"
 import styles from './Navbar.module.scss'
 
 const Navbar = props => {
     return ( 
-        <nav className={styles.navbar}>{props.children}</nav>
+        <nav className={`${props.theme === 'purple' ? styles.purple : null} ${styles.navbar}`}>{props.children}</nav>
      );
+}
+
+Navbar.propTypes = {
+    theme: PropTypes.string
 }
  
 export default Navbar;
