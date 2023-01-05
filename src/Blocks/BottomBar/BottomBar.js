cd5d00cc09b40f37b09c815a4da846ce38e9eb08
simonduckett <simonjduckett@hotmail.co.uk>
2023-01-05 16:26:21 +0000

    build mob bottom bar

--- src/Blocks/BottomBar/BottomBar.js
+++ src/Blocks/BottomBar/BottomBar.js
@@ -1,27 +1,32 @@
 import React from "react";
 import styles from "./BottomBar.module.scss"
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 import { Button, List, ListItem, NavToggle } from "../../components";
+import '../../global/styles/global.scss'
 
 const BottomBar = () => {
     const showQuoteNav = e => {
         e.target.classList.toggle(styles.active)
+        document.getElementById('mobileQuoteNav').classList.toggle('showNav')
     }
-    return ( 
-        <div className={styles.bottomBar}>
-            <Row>
-                <Col xs={6}>
-                    <Button onClick={showQuoteNav} customClass={styles.getQuotesBtn}></Button>
-                    <span className={styles.text}>Get Quotes</span>
-                </Col>
-                <Col xs={6}>
-                    <NavToggle customClass='mx-auto' navid='mainNav' />
-                    <span className={styles.text}>Menu</span>
-                </Col>
-            </Row>
-
+    return (
+        <>
+            <div className={styles.bottomBar}>
+                    <Row>
+                        <Col xs={6}>
+                            <Button onClick={showQuoteNav} customClass={styles.getQuotesBtn}></Button>
+                            <span className={styles.text}>Get Quotes</span>
+                        </Col>
+                        <Col xs={6}>
+                            <NavToggle customClass='mx-auto' navid='mainNav' />
+                            <span className={styles.text}>Menu</span>
+                        </Col>
+                    </Row>
+            </div>
+        
             <div id='mobileQuoteNav' className={styles.mobQuoteNav}>
-                <List> 
+                <List>
+                    <ListItem><strong>Get quotes for:</strong></ListItem>
                     <ListItem><a href="/quote-forms/conveyancing">Conveyancing</a></ListItem>
                     <ListItem><a href="/quote-forms/surveyors">Surveyors</a></ListItem>
                     <ListItem><a href="/surveyors/valuations">Valuations</a></ListItem>
@@ -32,7 +37,7 @@ const BottomBar = () => {
                     <ListItem><a href="/home-reports/get-quotes">Scottish Home Reports</a></ListItem>
                 </List>
             </div>
-        </div>
+        </>
      );
 }
  
